30f440c38111a163ca4fcd8c121a19e35992ae44
Danilo Rocha <danilo.rochaa@gmail.com>
2025-10-23 12:43:35 -0300

    Refactor do ProductController

--- src/main/java/com/danilo/auth/controllers/ProductController.java
+++ src/main/java/com/danilo/auth/controllers/ProductController.java
@@ -1,36 +1,40 @@
 package com.danilo.auth.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.danilo.auth.domain.product.Product;
 import com.danilo.auth.domain.product.ProductRequestDTO;
 import com.danilo.auth.domain.product.ProductResponseDTO;
 import com.danilo.auth.repositories.ProductRepository;
 
-import java.util.List;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 
-@RestController()
+@RestController
+@AllArgsConstructor
 @RequestMapping("product")
 public class ProductController {
 
-    @Autowired
-    ProductRepository repository;
+    private ProductRepository repository;
 
     @PostMapping
-    public ResponseEntity<?> postProduct(@RequestBody @Valid ProductRequestDTO body){
-        Product newProduct = new Product(body);
-
+    public ResponseEntity<?> postProduct(@RequestBody @Valid ProductRequestDTO body) {
+        var newProduct = new Product(body);
         this.repository.save(newProduct);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllProducts(){
-        List<ProductResponseDTO> productList = this.repository.findAll().stream().map(ProductResponseDTO::new).toList();
-
+    public ResponseEntity<?> getAllProducts() {
+        var productList = this.repository.findAll().stream()
+                .map(ProductResponseDTO::new)
+                .toList();
         return ResponseEntity.ok(productList);
     }
 }
